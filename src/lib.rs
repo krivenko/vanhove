a65f081e8e971ea58e001b06fa39a0d90216d30e
Igor Krivenko <iskrivenko@proton.me>
2026-08-13 17:33:45 +0200

    Implemented `integrate()` and `integrate_complex()`

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ mod util;
 use std::ops::{Add, Mul};
 use std::rc::Rc;
 
+use num_complex::Complex64;
+
 //
 // DiscreteDOS
 //
@@ -42,12 +44,9 @@ impl DiscreteDOS {
     }
 
     /// Make a discrete DOS with one resonance
-    pub fn one_resonance(eps: f64) -> DiscreteDOS {
+    pub fn one_resonance(eps: f64, weight: f64) -> DiscreteDOS {
         DiscreteDOS {
-            resonances: vec![Resonance {
-                eps,
-                weight: 1.0f64,
-            }],
+            resonances: vec![Resonance { eps, weight }],
             clean: 1,
             tol: f64::EPSILON,
         }
@@ -119,16 +118,6 @@ impl DiscreteDOS {
 // ContinuousDOS
 //
 
-/// Descriptor of an integrable singularity in DOS.
-#[derive(Clone)]
-pub struct Singularity {
-    /// Position of the singularity, Ω_p
-    position: f64,
-    /// Asymptotic behavior of the DOS near the singularity, S_p(ω).
-    /// To be chosen such that \lim_{ω → Ω_p} [A(ω) - S_p(ω)] = 0.
-    asymptotics: Rc<dyn Fn(f64) -> f64>,
-}
-
 /// Continuous density of states possibly containing integrable singularities.
 /// The DOS has the form A(ω) = R(ω) + ∑_p S_p(ω) for ω ∈ [ω_{min}, ω_{max}]
 /// and zero otherwise. R(ω) is a smooth function and each S_p(ω) has one isolated
@@ -138,10 +127,12 @@ pub trait ContinuousDOS {
     fn support(&self) -> (f64, f64);
     /// Regular part of the DOS, R(ω)
     fn regular(&self, omega: f64) -> f64;
-    /// Singular contributions S_p(ω), if any
-    fn singularities(&self) -> &Singularity;
+    /// Positions of singularities
+    fn sing_pos(&self) -> &[f64];
+    /// Singular contributions S_p(ω)
+    fn asymptotics(&self) -> &[Rc<dyn Fn(f64) -> f64>];
     /// Analytically derived values of ∫S_p(ω)dω over [ω_{min}, ω_{max}].
-    fn sing_int(&self) -> [f64];
+    fn asympt_int(&self) -> &[f64];
 }
 
 // Density of states as a weighted sum of discrete resonances and continuous contributions
@@ -205,6 +196,64 @@ impl Add for DensityOfStates {
     }
 }
 
+//
+// DOS integration
+//
+
+impl DensityOfStates {
+    /// Given a density of states A(ω) and a real-valued function f(ω),
+    /// compute the integral ∫A(ω)f(ω)dω.
+    pub fn integrate<F: Fn(f64) -> f64>(&self, f: F, tol: Option<f64>) -> f64 {
+        let mut result = 0.0f64;
+
+        // Discrete spectral contributions
+        for r in &self.discrete.resonances {
+            result += r.weight * f(r.eps);
+        }
+
+        // Continuous spectral contributions
+        // ∫A(ω)f(ω)dω = ∫R(ω)f(ω)dω + ∑_p ∫S_p(ω)[f(ω) - f(Ω_p)]dω + ∑_p f(Ω_p) ∫S_p(ω)dω
+        let tol = tol.unwrap_or(1e-10);
+        for (cdos, w) in &self.continuous {
+            let mut res_contrib = 0.0f64;
+            let (omega_min, omega_max) = cdos.support();
+
+            // Integrate the regular part, ∫R(ω)f(ω)dω
+            res_contrib += util::bilby_integrate(
+                |omega| cdos.regular(omega) * f(omega),
+                omega_min,
+                omega_max,
+                tol,
+            );
+
+            // Add integrals of the asymptotics
+            for (p, omega_p) in cdos.sing_pos().into_iter().enumerate() {
+                // ∫S_p(ω)[f(ω) - f(Ω_p)]dω
+                let s_p = &cdos.asymptotics()[p];
+                let f_p = f(*omega_p);
+                res_contrib += util::bilby_integrate(
+                    |omega| s_p(omega) * (f(omega) - f_p),
+                    omega_min,
+                    omega_max,
+                    tol,
+                );
+                // f(Ω_p) ∫S_p(ω)dω
+                res_contrib += cdos.asympt_int()[p] * f_p;
+            }
+
+            result += w * res_contrib;
+        }
+        result
+    }
+
+    /// Given a density of states A(ω) and a complex-valued function f(ω),
+    /// compute the integral ∫A(ω)f(ω)dω
+    pub fn integrate_complex<F: Fn(f64) -> Complex64>(&self, f: F) -> Complex64 {
+        self.integrate(|omega| f(omega).re, None)
+            + Complex64::I * self.integrate(|omega| f(omega).im, None)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -215,7 +264,7 @@ mod tests {
         assert_eq!(dos.resonances().len(), 0);
         assert_eq!(dos.norm(), 0.0);
 
-        let mut dos = DiscreteDOS::one_resonance(2.0);
+        let mut dos = DiscreteDOS::one_resonance(2.0, 1.0);
         assert_eq!(dos.resonances().len(), 1);
         assert_eq!(dos.resonances()[0].eps, 2.0);
         assert_eq!(dos.resonances()[0].weight, 1.0);
--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,8 @@
 /// Utility functions
+use bilby::{
+    adaptive_integrate, integrate_infinite, integrate_semi_infinite_lower,
+    integrate_semi_infinite_upper,
+};
 
 /// Kahan-Babuška-Neumaier summation algorithm
 pub fn kahan_babushka_neumaier_sum<I: Iterator<Item = f64>>(input: I) -> f64 {
@@ -15,16 +19,69 @@ pub fn kahan_babushka_neumaier_sum<I: Iterator<Item = f64>>(input: I) -> f64 {
     sum + c
 }
 
+/// Call a bilby adaptive integration function depending on the integration limits
+/// and unwrap the result.
+pub fn bilby_integrate<F: Fn(f64) -> f64>(f: F, a: f64, b: f64, tol: f64) -> f64 {
+    let a_inf = a.is_infinite();
+    let b_inf = b.is_infinite();
+    // Choose a bilby call depending on the integration limits
+    match (a_inf, b_inf) {
+        (false, false) => adaptive_integrate(f, a, b, tol),
+        (false, true) => {
+            assert!(b > 0.0);
+            integrate_semi_infinite_upper(f, a, tol)
+        }
+        (true, false) => {
+            assert!(a < 0.0);
+            integrate_semi_infinite_lower(f, b, tol)
+        }
+        (true, true) => {
+            assert!(a < 0.0 && b > 0.0);
+            integrate_infinite(f, tol)
+        }
+    }
+    .unwrap()
+    .value
+}
+
 #[cfg(test)]
 mod tests {
     use crate::util;
+    use approx::assert_abs_diff_eq;
 
     #[test]
     fn kahan_babushka_neumaier_sum() {
         let v = vec![10000.0f64, 3.14159f64, 2.71828f64];
-        assert_eq!(
+        assert_abs_diff_eq!(
             util::kahan_babushka_neumaier_sum(v.into_iter()),
-            10005.85987
+            10005.85987,
+            epsilon = 1e-12
+        );
+    }
+
+    #[test]
+    fn bilby_integrate() {
+        let pi = std::f64::consts::PI;
+        let inf = std::f64::INFINITY;
+        assert_abs_diff_eq!(
+            util::bilby_integrate(move |x| x.cos() * x.cos(), -pi, pi, 1e-12),
+            pi,
+            epsilon = 1e-12
+        );
+        assert_abs_diff_eq!(
+            util::bilby_integrate(move |x| (-x * x / 2.0).exp(), -inf, inf, 1e-12),
+            (2.0 * pi).sqrt(),
+            epsilon = 1e-12
+        );
+        assert_abs_diff_eq!(
+            util::bilby_integrate(move |x| (2.0 * x).exp(), -inf, 0.0, 1e-12),
+            0.5,
+            epsilon = 1e-12
+        );
+        assert_abs_diff_eq!(
+            util::bilby_integrate(move |x| (-2.0 * x).exp(), 0.0, inf, 1e-12),
+            0.5,
+            epsilon = 1e-12
         );
     }
 }
